ce2ce40b3ff34067df429e992ce81a5b74b55f85
Jani Tarvainen <janit@iki.fi>
2017-01-21 12:26:26 +0200

    Added axios lib for http, some other smaller work

--- src/AppBundle/Resources/public/js/typescript/app.ts
+++ src/AppBundle/Resources/public/js/typescript/app.ts
@@ -1,17 +1,20 @@
 
 /// <reference path="appstate.d.ts" />
+/// <reference path="axios.d.ts" />
 
 
 async function initTsApp(){
 
 
-    let items = await loadContent();
+    let apiResponse = await axios.get('/api');
+
+    let apartments = apiResponse.data.apartments;
 
     var output = '';
 
-    for(let j in items){
+    for(let j in apartments){
 
-        let apartment = items[j];
+        let apartment = apartments[j];
 
         output += `
                     <tr>
@@ -32,22 +35,5 @@ async function initTsApp(){
     newRows.innerHTML = output;
 
     document.querySelector('#ts-app').appendChild(newRows);
-
-}
-
-function loadContent(){
-
-
-    return new Promise( function(resolve, reject){
-
-        setTimeout( () => {
-
-            let items = JSON.parse('{"apartments":[{"id":1,"streetaddress":"9965 Kulas Green","city":"Port Arvilla","zipcode":"09978","country":"Libyan Arab Jamahiriya","buildyear":1977,"size":68},{"id":2,"streetaddress":"99892 Kemmer Station Suite 134","city":"Jaskolskiview","zipcode":"98395-3141","country":"Fiji","buildyear":1977,"size":68},{"id":3,"streetaddress":"96447 Jada Inlet Apt. 020","city":"Floland","zipcode":"05959","country":"McDonald Islands","buildyear":1977,"size":68}]}');
-
-            resolve( items.apartments );
-
-        },1000);
-
-    } );
 
 }
